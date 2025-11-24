84640abfb403b7930caad092c77a4743f6c3e9fb
SHEIK REZWAN ALI <128757563+rezwanali2003@users.noreply.github.com>
2025-11-24 16:25:28 +0530

    Remove trailing slash from API base URL

--- bitly/bitly/next.config.mjs
+++ bitly/bitly/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_BASE_URL: "https://full-stack-developer-position-at-aganitha.onrender.com/",
+    NEXT_PUBLIC_API_BASE_URL: "https://full-stack-developer-position-at-aganitha.onrender.com"
   },
 };
 
